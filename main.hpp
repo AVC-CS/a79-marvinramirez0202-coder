ab5b0bd605298664b55390a1519984b2c24f359d
marvinramirez0202-coder <marvinramirez0202@gmail.com>
2026-05-12 18:55:25 -0700

    Updated main.hpp

--- main.hpp
+++ main.hpp
@@ -21,16 +21,40 @@ void printVector(vector<int>);
 void makeVector(vector<int> &number)
 {
     // TODO: fill the vector with random integers in [0, 20), then sort it.
+    for(int i = 0; i < number.size(); i++){
+        number[i] = rand() % 20;
+    }
+    sort(number.begin(), number.end());
 }
 
 void insertVector(vector<int> &number, int usernum)
 {
     // TODO: insert usernum into the sorted vector while keeping it sorted.
+    
+    for(auto it = number.begin(); it != number.end(); it++){
+        if(*it > usernum){
+            number.insert(it, usernum);
+            return;
+        }
+    }
+    number.insert(number.end(), usernum);
+
 }
 
 int deleteVector(vector<int> &number, int usernum)
 {
     // TODO: erase EVERY occurrence of usernum from the vector.
+    vector<int>::iterator it;
+    int count = 0;
+    it = find(number.begin(), number.end(), usernum);
+    while(it != number.end()){
+        number.erase(it);
+        count++;
+        it = find(number.begin(), number.end(), usernum);
+    }
+    if(count == 0)
+        return -1;
+    return count;
 }
 
 int getInput(void)
